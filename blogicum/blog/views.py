39bd63bd242f2fce371e21209cd8a7ce91686c1b
Andrey Martynov <vkusno01@yandex.ru>
2024-07-22 20:49:38 +0300

    -

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, \
